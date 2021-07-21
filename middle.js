738251e2d789b0470071d942c20fb128f35b283c
cupokyle <kyleforsythcomedy@gmail.com>
2021-07-21 17:51:41 +0000

    add countOnly.js and eslint two files

--- middle.js
+++ middle.js
@@ -46,4 +46,5 @@ assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 assertArraysEqual(middle(["Bob", "Peter", "Hank", 100, "Tom", "Lester"]), ["Hank", 100]);
 assertArraysEqual(middle([1, 1, 1]), [1]);
+assertArraysEqual(middle([2, 1]), []);
 
