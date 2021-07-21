b61b511124e1ecf8526e45712b0bf1ad6b4518a0
cupokyle <kyleforsythcomedy@gmail.com>
2021-07-21 13:41:54 +0000

    eslint middle

--- middle.js
+++ middle.js
@@ -25,25 +25,25 @@ const middle = function(array) {
   //Create new array
   let solution = [];
   //If less than 3 elements, return empty
-  if (array.length < 3){
+  if (array.length < 3) {
     return solution;
   }
   //Find the middle index
   const middleIndex = (array.length - 1) / 2;
   //If there are an even number of elements, return both middle elements
-  if(array.length % 2 === 0){
+  if (array.length % 2 === 0) {
     let mid1 = Math.floor(middleIndex);
     let mid2 = Math.round(middleIndex);
-    solution.push(array[mid1], array[mid2])
+    solution.push(array[mid1], array[mid2]);
   } else {
   // Odd: just the middle element.
-    solution.push(array[middleIndex])
+    solution.push(array[middleIndex]);
   }
   return solution;
-}
+};
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle(["Bob", "Peter", "Hank", 100, "Tom", "Lester"]), ["Hank", 100])
-assertArraysEqual(middle([1, 1, 1]), [1])
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle(["Bob", "Peter", "Hank", 100, "Tom", "Lester"]), ["Hank", 100]);
+assertArraysEqual(middle([1, 1, 1]), [1]);
 
