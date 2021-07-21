53d4ff2e85ee4382b5c267adb691a843bcf8d0b7
cupokyle <kyleforsythcomedy@gmail.com>
2021-07-21 18:56:01 +0000

    add findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,6 +6,24 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const findKeyByValue = function() {
-  //
-}
+const findKeyByValue = function(object, value) {
+  for (let key of Object.keys(object)) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKeyByValue({a:1, b:2, c:3, d:2}, 1), "a");
+assertEqual(findKeyByValue({a:1, b:2, c:3, d:2}, 2), "b");
+assertEqual(findKeyByValue({a:"john", b:"johnny", c:"johnathon", d:"j"}, "johnathon"), "c");
+assertEqual(findKeyByValue({a:1, b:2, c:3, d:"pineapple"}, "pineapple"), "d");
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
