b4946aabdf4645fdb920463e0b6d66f832e08280
cupokyle <kyleforsythcomedy@gmail.com>
2021-07-21 18:15:55 +0000

    manual test added

--- countLetters.js
+++ countLetters.js
@@ -7,5 +7,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const countLetters = function(string) {
-  //
-}
+  let solution = {};
+  for (let letter of string) {
+    if (solution[letter]) {
+      solution[letter]++;
+    } else {
+      if (letter !== ' ') {
+        solution[letter] = 1;
+      }
+    }
+  }
+  console.log(solution);
+  return solution;
+};
+
+console.log(countLetters("kyle kyle kyle le kyle"));
